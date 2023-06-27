f06ca2c1d06c0c274deba8b4e376d6b8732a459e
Rigidsetya <panjigangga23@gmail.com>
2023-06-27 19:20:54 +0700

    update

--- Webgis_0137.py
+++ Webgis_0137.py
@@ -1,4 +1,19 @@
 import folium
+import json
+import requests
 
-maps = folium.Map(location=[-6.362904577664523, 107.17147603051227], zoom_start=10, tiles="CartoDB positron")
-maps.save("Kab_Bekasi.html")
+url = (
+    "https://raw.githubusercontent.com/Rigidsetya/Folium_Web-Python/main/ADMIN_KAB-KOTA-BEKASI_AR.geojson"
+)
+
+m = folium.Map(
+    location=[-6.365634233115968, 107.17628254886996],
+    tiles="cartodbpositron",
+    zoom_start=10,
+)
+
+folium.GeoJson(url, name="geojson").add_to(m)
+
+folium.LayerControl().add_to(m)
+
+m.save("Kab_Bekasi.html")
